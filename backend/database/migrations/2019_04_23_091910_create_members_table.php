46945db6a51b98ad9fb275bc00bfabb173fd030a
gaoxiang <4008353@qq.com>
2019-04-23 18:35:07 +0800

    fixed members

---
+++ backend/database/migrations/2019_04_23_091910_create_members_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMembersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('members', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('opienid')->nullable(false);
+            $table->string('unionid')->nullable(false);
+            $table->string('nick_name')->comment('用户昵称');
+            $table->tinyInteger('gender')->default(0)->nullable(false)->comment('性别 0：未知、1：男、2：女');
+            $table->string('avatar')->comment('头像');
+            $table->string('country', 100)->comment('地区');
+            $table->string('province', 100)->comment('省');
+            $table->string('city', 100)->comment('市');
+            $table->string('platform', 50)->default('wechat')->nullable(false)->comment('平台 wechat:微信');
+            $table->integer('total_draw')->nullable(false)->comment('单日最大抽奖次数');
+            $table->integer('base_draw')->nullable(false)->comment('基本抽奖次数');
+            $table->tinyInteger('status')->default(1)->nullable(false)->comment('状态 0:禁止使用 1:正常');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('members');
+    }
+}
---
+++ backend/database/migrations/2019_04_23_094322_create_member_cards_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMemberCardsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('member_cards', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('fk_member_id')->nullable(false);
+            $table->integer('fk_card_id')->nullable(false);
+            $table->tinyInteger('delete')->default(0)->nullable(false)->comment('是否删除 0:否 1:是');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('member_cards');
+    }
+}
